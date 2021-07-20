44ca70014b6dad5e5fc19cc0247bd1872ca28918
Ubuntu <ubuntu@bkj-faq-0.kamujapadl4urjrftjxrpo5krd.bx.internal.cloudapp.net>
2021-07-20 02:34:25 +0000

    versions

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ conda activate uclasm_env
 # ?? versions
 pip install numpy==1.21.1
 pip install scipy==1.7.0
-pip install pandas==1.3.0
+pip install pandas==1.0.1
 pip install tqdm==4.61.2
 pip install matplotlib==3.4.2
 pip install networkx==2.5.1
